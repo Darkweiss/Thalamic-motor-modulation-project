aa2a0ea50ef8fb04df775f76b04778441cfb7f3e
Luka <gantar.luka9@gmail.com>
2021-09-07 16:59:21 +0100

    Comments

---
+++ Electrophysiology/Offline_synchronisation.m
@@ -0,0 +1,5 @@
+%%This code will set bin sizes of 2 ms (whisker cam frame rate) for
+%%electrophysiology and assign an SSM corrected 3D pose to each of these
+%%bins
+%%I imagine a data structure holding the spike numbers for each bin, the
+%%2D whisker location described by whisker tracking and a 3D pose 
