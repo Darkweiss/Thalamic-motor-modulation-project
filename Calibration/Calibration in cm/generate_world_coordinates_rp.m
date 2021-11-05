b68c355b1f32bf37c8f70ee7aa230240ae8a65f4
Luka <gantar.luka9@gmail.com>
2021-11-05 15:27:38 +0000

    Added camera calibration in cm

---
+++ Calibration/Calibration in cm/generate_world_coordinates_rp.m
@@ -0,0 +1,26 @@
+function[] = generate_world_coordinates_rp()
+%generate real world coordinates for Lego(R) objects
+%z coordinates from camera 1-5
+h0 = [0.17 3.0 4.97 7.85 9.87]; 
+Nh = length(h0);
+%x,y coordinates camera 1-5
+
+x0 = [0 4.2  9.6  15.2  20 24.8  0 4.2  9.6  15.2  20 24.8 0 4.2  9.6  15.2  20 24.8 0 4.2  9.6  15.2  20 24.8]; x0 = x0-mean(x0); Nx0 = length(x0); 
+y0 = [0 0  0   0   0  0   9.6 9.6 9.6  9.6  9.6 9.6 15.2 15.2 15.2  15.2  15.2 15.2 24.8 24.8 24.8  24.8  24.8 24.8]; y0 = y0-mean(y0); Ny0 = length(y0);
+%y0 = [7 13 20 26 7 13 20 26]; y0 = y0-mean(y0); Ny0 = length(y0); 
+%x0 = [13 13 13 13 20 20 20 20]; x0 = x0-mean(x0); Nx0 = length(x0);
+
+Np = Nx0*Nh;
+%generate all points
+X = [];
+for h = 1:Nh
+    for n = 1:Nx0
+        X = [X [x0(n); y0(n); h0(h); 1]];
+    end
+end
+
+%fig
+figure; plot3(X(1,:),X(2,:),X(3,:),'.','MarkerSize',15);
+%xlim([-10 10]); ylim([-10 10]); zlim([-1 15]); 
+%save
+save('world_coordinates_rp','X');
--- Electrophysiology/Analysis of spiking data/Poisson GLM.py
+++ Electrophysiology/Analysis of spiking data/Poisson GLM.py
@@ -5,7 +5,47 @@ Created on Tue Nov  2 13:50:47 2021
 @author: mfbx5lg2
 """
 
+"""
+Created on Tue Nov  2 14:58:12 2021
+
+@author: mfbx5lg2
+"""
+
+
+def plot_spikes_with_prediction(
+    spikes, predicted_spikes, dt, nt, t0, **kws):
+  """Plot actual and predicted spike counts.
+
+  Args:
+    spikes (1D array): Vector of actual spike counts
+    predicted_spikes (1D array): Vector of predicted spike counts
+    dt (number): Duration of each time bin.
+    nt (number): Number of time bins to plot
+    t0 (number): Index of first time bin to plot.
+    kws: Pass additional keyword arguments to plot()
+
+  """
+  t = np.arange(t0, t0 + nt) * dt
+
+  f, ax = plt.subplots()
+  lines = ax.stem(t, spikes[:nt], use_line_collection=True)
+  plt.setp(lines, color=".5")
+  lines[-1].set_zorder(1)
+  kws.setdefault("linewidth", 3)
+  yhat, = ax.plot(t, predicted_spikes[:nt], **kws)
+  ax.set(
+      xlabel="Time (s)",
+      ylabel="Spikes",
+  )
+  ax.yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+  ax.legend([lines[0], yhat], ["Spikes", "Predicted"])
+
+  plt.show()
+
+
 from sklearn.linear_model import PoissonRegressor
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 import numpy as np
 
 #import the data
@@ -25,9 +65,19 @@ X = X[selected_feature,:]
 X = np.transpose(X)
 X = X.reshape(-1, 1)
 
+#split into training and test set
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33,random_state = 42)
 
 
-poission_model = PoissonRegressor()
+
+
+poission_model = PoissonRegressor(alpha = 0)
+#poission_model.fit(X_train,y_train)
+#y_pred = poission_model.predict(X_test)
+
 poission_model.fit(X,y)
+y_pred = poission_model.predict(X)
 
-print(32)
+
+
+plot_spikes_with_prediction(y, y_pred, 0.3,2000,0)
